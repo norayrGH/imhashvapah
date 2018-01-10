cd6c7232adc6c83e831966456ee8d342a0664629
noro741 <norayr.gharibyan.java@gmail.com>
2018-01-10 19:07:06 +0400

    to be continue

--- src/main/resources/static/js/employeeCreate.js
+++ src/main/resources/static/js/employeeCreate.js
@@ -1,43 +1,75 @@
-var  cityes = $("<select>").attr({id:"selectCityes"}).append($("<option>").attr({selected:"selected",disabled :"disabled "}).text(" Ընտրեք քաղաքը "));
-var address={city:null,region:null,stritFleet:null};
+/*var  cityes = $("<select>").attr({ required:" required",id:"selectCityes"}).append($("<option>").attr({selected:"selected",disabled :"disabled "}).text(" Ընտրեք քաղաքը "));
+var  regionselect = $("<select>").attr({required:" required",id:"regionSelect"}).append($("<option>").attr({selected:"selected",disabled :"disabled "}).text(" Ընտրեք Մարզերը "));
+var  stritFleet = $("<input>").attr({id:"stritFleet",placeholder:"փողոցի անուն , շենք կամ տուն "});*/
+var  cityes = $("#selectCityes");
+var  regionselect = $("#regionSelect");
+var  stritFleet = $("#stritFleet");
+
+
+var address={stritFleet:"",region:"",city:""};
+var textaddress="";
 
 $.each( addresses, function( key, value ){
     cityes.append($("<option>").attr({class:"city"}).text(key));
 });
 
-$("#cityes").append(cityes);
 
+$("#stritFleet").focusout(function () {
 
+    address["stritFleet"]=$("#stritFleet").val();
+    $.each( address, function( key , value ){
+        textaddress+=value;
+        if(key != "city" )
+            textaddress+=",";
+    });
+    $("#juridicalAddress").val(textaddress);
 
+    textaddress="";
+
+});
 
 $("#selectCityes").change(function () {
-    $("#regions").empty();
-      address["city"]=null;
+    regionselect.empty();
+
+      address["city"]="";
       var city = $("#selectCityes").val();
       address["city"] = city;
       var regions = addresses[city];
-      var regionselect = $("<select>").attr("id","regionSelect").append($("<option>").attr({selected:"selected",disabled :"disabled "}).text(" Ընտրեք Մարզերը "));
     $.each( regions, function( key,value ){
         regionselect.append($("<option>").attr({class:"region"}).text(value));
     });
-    $("#regions").append(regionselect);
 
-    console.log(address);
-    $("#juridicalAddress").text(JSON.stringify(address));
 
+    $.each( address, function( key, value ){
+        textaddress+=value;
+        if(key != "city" )
+            textaddress+=",";
+
+    });
+
+    $("#juridicalAddress").val(textaddress);
+textaddress="";
 
 });
 
 
 
 $("#regionSelect").change(function () {
-    alert("ok");
-   address["region"]=null;
+
+    address["region"]="";
     console.log($("#regionSelect").val());
     address["region"] = $("#regionSelect").val();
 
     console.log(address);
-    $("#juridicalAddress").text(JSON.stringify(address));
+
+    $.each( address, function( key, value ){
+        textaddress+=value;
+        if(key != "city" )
+        textaddress+=",";
+    });
+    $("#juridicalAddress").val(textaddress);
+
+    textaddress="";
 
 
 });
