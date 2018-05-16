37a8a396ad9070b36b1e72b19e9703741e865e02
Norayr Gharibyan <Norayr.Gharibyan@ad.ucom.am>
2018-05-16 15:10:40 +0400

    cashOut Done

--- src/main/java/com/example/imhashvapahversion1/version1/Entity/cash/walettypes/cashOut/CashOutForBankSpending.java
+++ src/main/java/com/example/imhashvapahversion1/version1/Entity/cash/walettypes/cashOut/CashOutForBankSpending.java
@@ -10,9 +10,43 @@ public class CashOutForBankSpending {
     @Id
     @GeneratedValue
     private Long id;
-    @ManyToOne
-    private Organization organization;
+
+
     @OneToOne(cascade = javax.persistence.CascadeType.ALL)
     @Valid
     private WalletOut walletOut;
+    @ManyToOne
+    private Organization organization;
+
+    public CashOutForBankSpending() {
+    }
+
+    public CashOutForBankSpending(WalletOut walletOut, Organization organization) {
+        this.walletOut = walletOut;
+        this.organization = organization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WalletOut getWalletOut() {
+        return walletOut;
+    }
+
+    public void setWalletOut(WalletOut walletOut) {
+        this.walletOut = walletOut;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
 }
